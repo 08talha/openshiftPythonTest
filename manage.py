41d8bbdd1dcf67878ae61b883f0725e02c72c3cf
Muhammed Talha <talha.sahin@bil.omu.edu.tr>
2023-02-21 12:59:04 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
 from flask import Flask
 import os
-#import socket
+import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     html = """Hello World!
-    """
+    Hostname: {hostname} """
     #Hostname: {hostname}
-    #return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
-    return html
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+    #return html
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8082)
